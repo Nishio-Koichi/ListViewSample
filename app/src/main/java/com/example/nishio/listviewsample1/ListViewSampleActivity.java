09b9fdf68b2cc324dcff6d1897c15fdff30bdc28
Nishio <16010415@anabuki-college.ac.jp>
2018-06-06 09:45:24 +0900

    p107

--- app/src/main/java/com/example/nishio/listviewsample1/ListViewSampleActivity.java
+++ app/src/main/java/com/example/nishio/listviewsample1/ListViewSampleActivity.java
@@ -2,6 +2,10 @@ package com.example.nishio.listviewsample1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 public class ListViewSampleActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class ListViewSampleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view_sample);
+    }
+    private class ListItemClickListener implements AdapterView.OnItemClickListener{
+        @Override
+        public void onItemClick(AdapterView<?> parent , View view, int position, long id){
+            //タップされた定食名を取得。
+            String item =(String) parent.getItemAtPosition(position);
+            //トーストで表示する文字列を作成。
+            String show = "あなたが選んだ定食:" + item;
+            Toast.makeText(ListViewSampleActivity.this, show, Toast.LENGTH_LONG).show();
+
+        }
     }
 }
